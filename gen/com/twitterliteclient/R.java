d7181a5eb87848a2617a370d910458f24e16003f
adriangonzy <adrian.gonzy@gmail.com>
2013-11-05 00:46:05 +0100

    started loginActivity

--- gen/com/twitterliteclient/R.java
+++ gen/com/twitterliteclient/R.java
@@ -24,7 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int email=0x7f080001;
+        public static final int login=0x7f080000;
+        public static final int submit=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -35,7 +38,11 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int create=0x7f050006;
+        public static final int create_usr=0x7f050004;
+        public static final int e_mail=0x7f050005;
         public static final int hello_world=0x7f050002;
+        public static final int login=0x7f050003;
     }
     public static final class style {
         /** 
