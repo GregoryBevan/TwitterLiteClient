22da128f248e14d918f01b19f87475fe75df604b
Grégory <grego@elgregos.com>
2014-04-07 22:38:04 +0200

    Update DEFAULT_ROOT_URL

--- src/com/appspot/twitterlitesample/message/Message.java
+++ src/com/appspot/twitterlitesample/message/Message.java
@@ -57,7 +57,7 @@ public class Message extends com.google.api.client.googleapis.services.json.Abst
    *
    * @since 1.7
    */
-  public static final String DEFAULT_ROOT_URL = "https://twitterlitesample.appspot.com/_ah/api/";
+  public static final String DEFAULT_ROOT_URL = "https://mytwitterlite.appspot.com/_ah/api/";
 
   /**
    * The default encoded service path of the service. This is determined when the library is
